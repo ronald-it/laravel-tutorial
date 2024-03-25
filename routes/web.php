4101839b2940957055f5d0a2d2aa25ccf47cfe58
ronald-it <ronald.cirgofer@hotmail.com>
2024-03-25 14:03:38 +0100

    Displaying the Link Submission Form is nu ook af.

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome', ['links' => $links]);
 });
 
+Route::get('/submit', function () {
+    return view('submit');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
