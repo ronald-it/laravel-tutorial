15d02f99285817abc82518f68837b31df9d8db20
ronald-it <ronald.cirgofer@hotmail.com>
2024-03-25 12:34:08 +0100

    Routing and views onderdeel voltooid.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+ 
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
